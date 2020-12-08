d770fa376e2da295745853e2d5c3175fa9463584
bluejok3 <44967836+bluejok3@users.noreply.github.com>
2020-12-08 20:34:15 +0100

    Update Python_M1.py

--- Python_M1.py
+++ Python_M1.py
@@ -176,13 +176,33 @@ print(list_triee)
 
 
 # exercice 4 questions 1 à 3
-couleurs=["Pique","Trèfle","Carreau","Coeur"]
-valeurs =["2","3","4","5","6","7","8","9","10","valet","dame","roi","as"]
-for x in couleurs :
-   for y in valeurs:
-      print (x+" "+y)
+Couleurs = ["Pique", "Trefle", "Carreau", "Coeur"]
+valeurs = [2, 3, 4, 5, 6, 7, 8, 9, 10, "valet", "dame", "roi", "as"]
+deck = []
+for C in Couleurs:
+   for v in valeurs:
+       card = str(v) + " " + str(C)
+       deck.append(card)
+#print(deck)
 
 from random import shuffle
+shuffle(deck)
+print(deck)
+
+joueur1 = []
+joueur2 = []
+joueur3 = []
+joueur4 = []
+for index in range(13):
+    joueur1.append(deck[index])
+    joueur2.append(deck[index + 1])
+    joueur3.append(deck[index + 2])
+    joueur4.append(deck[index + 3])
+
+print(" J1 = " + str(joueur1))
+print(" J2 = " + str(joueur2))
+print(" J3 = " + str(joueur3))
+print(" J4 = " + str(joueur4))
 
 
 # In[29]:
@@ -214,24 +234,149 @@ with open("diamonds.csv", "r")as f:
 diamants_prix[:20]
 
 
-# In[53]:
+# In[5]:
 
 
-#exercice 6 question 1, 2 et 3
-prenom= input ('renseignez votre prénom')
-nom= input ('renseignez votre nom')
-matricule= input ('renseignez votre matricule')
-informationEleve = (prenom, nom, matricule)
+#exercice 6 question 1
+infoEleves = input("Entrer prenom, nom et matricule étudiant séparés par un espace : ")
+list = infoEleves.split(" ")
+lepie= tuple(list)
+print(lepie)
+main_list = []
 
-while matricule != FIN:
 
-#retourne les prénoms, noms et matricules des élèves
-def retourne_liste_etudiant():
-    return "prenom", "nom", "matricule"
+# In[6]:
+
+
+#exercice 6 question 2 et 3
+while True:
+    choix = input("Entrer prénom, nom et matricule étudiant séparés par un espace : (FIN pour finir) ")
+    if choix == "FIN":
+        break
+    else:
+        list = choix.split(" ")
+        matricule = tuple(list)
+        main_list.append(matricule)
+
+for etudiant in main_list:
+    print(etudiant[0])
+
+
+# In[7]:
+
+
+#exercice 7 questions 1 à 3
+fr_en = {
+    "jardin": "garden",
+    "voiture": "car",
+    "soleil": "sun",
+    "cerveau": "brain"
+    }
+for cle, valeur in fr_en.items():
+    if cle == "cerveau":
+        print(valeur)
+    else:
+        pass
+
+
+# In[8]:
+
+
+#exercice 7 question 4
+en_fr = {}
+for cle, valeur in fr_en.items():
+    en_fr[valeur] = cle  # assigne cette cle cette valeur
+print(en_fr)
+
+
+# In[10]:
+
+
+#exercice 7 question 5
+if "brain" in (en_fr): # par défaut dans les clés pour values .values
+    print("la traduction est la")
+
+
+# In[12]:
+
+
+#exercice 7 question 6
+for cle, values in en_fr.items():
+    if en_fr[cle] == "cerveau":
+        print("clé = " + cle, "valeur = " + values)
+
+
+# In[13]:
+
+
+#exercice 7 question 7 à 10
+new = {
+    "jardin": ["garden", "gardenne"],
+    "voiture": ["car", "carre"],
+    "soleil": ["sun", "sunne"],
+    "cerveau": ["brain", "brainne"],
+    "chemin" : ["path", "way"]
+    }
+
+for cle, values in new.items():
+    if cle == "chemin":
+        print ("la deuxième traducion du mot chemin est : " + values[1])
+
+del new["chemin"]
+print(new)
+
+
+# In[17]:
+
+
+#exercice 8 questions 1 et 2
+etudiant = {}
+
+while True:
+     choix = input("Entrer le prénom, nom et matricule étudiant séparés par un espace : (FIN pour finir) ")
+     if choix == "FIN":
+         break
+     else:
+         list = choix.split(" ")
+         lepie = tuple(list)
+         count = 0
+         for cle in etudiant.keys():
+             if lepie[1] in cle:
+                 count += 1
+         if count > 0:
+             new_key = montuple[1] + str(count)
+             etudiant[new_key] = lepie
+         else:
+             etudiant[lepie[1]] = lepie
+
+print(etudiant)
+
+
+# In[18]:
+
+
+#exercice 8 question 3
+for keys in etudiant.keys():
+    print("Nom étudiant : " + keys)
+
+
+# In[19]:
+
+
+#exercice 8 question 4
+if "Obama" in (etudiant):
+    print(etudiant["Obama"])
+
+
+# In[20]:
+
+
+#exercice 8 question 5
+
+for cle, values in etudiant.items():
+    if values[2] == "12345678":
+        print("cle " + str(cle), "valeur " + str(values))
 
 
 # In[ ]:
-
-
-
 
